038853681eae9d2dfc8170e3f1592b4c1cfe5cda
russmanr4 <russman.riverar4@gmail.com>
2022-11-04 09:13:38 -0500

    login y registrate original

--- app/src/main/java/com/example/kitkat/MainActivity.java
+++ app/src/main/java/com/example/kitkat/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.kitkat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mTextViewRegister;
+    TextInputEditText mTextInputEditTextEmail;
+    TextInputEditText mTextInputEditTextPassword; /*declara variable */
+    Button mButtonLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextViewRegister=findViewById(R.id.TextViewRegister); /*busca el identificador */
+
+        mTextInputEditTextEmail = findViewById(R.id.textInputEditTextEmail);
+        mTextInputEditTextPassword= findViewById(R.id.textInputEditTextPassword);
+        mButtonLogin = findViewById(R.id.btnlogin);
+
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+
+
+
+        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void login() {
+        String email = mTextInputEditTextEmail.getText().toString();
+        String password = mTextInputEditTextPassword.getText().toString();
+        Log.d("campo", "email "  +  email);
+        Log.d("campo" , "password "   +  password);
+
     }
 }
---
+++ app/src/main/java/com/example/kitkat/RegisterActivity.java
@@ -0,0 +1,31 @@
+package com.example.kitkat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    CircleImageView mCircleImageViewBack;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mCircleImageViewBack=findViewById(R.id.circleimageback);
+        mCircleImageViewBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+
+            }
+        });
+    }
+
+}

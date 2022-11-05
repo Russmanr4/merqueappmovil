0828b9fa6c9cee42727666e6a8149dd89f163e6a
russmanr4 <russman.riverar4@gmail.com>
2022-11-05 10:56:32 -0500

    Actualizando validando campor de Registro

--- app/src/main/java/com/example/kitkat/RegisterActivity.java
+++ app/src/main/java/com/example/kitkat/RegisterActivity.java
@@ -4,13 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RegisterActivity extends AppCompatActivity {
 
     CircleImageView mCircleImageViewBack;
-
+    TextInputEditText mTextInputEditTextUsername;
+    TextInputEditText mTextInputEditTextEmailR;
+    TextInputEditText mTextInputEditTextPasswordR;
+    TextInputEditText mTextInputEditTextConfirmPassword;
+    Button mButtonRegister;
 
 
     @Override
@@ -18,7 +29,22 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        //instancias de campo de Registro
         mCircleImageViewBack=findViewById(R.id.circleimageback);
+        mTextInputEditTextUsername= findViewById(R.id.textInputEditTextUsername);
+        mTextInputEditTextEmailR= findViewById(R.id.textInputEditTextEmailR);
+        mTextInputEditTextPasswordR= findViewById(R.id.textInputEditTextPasswordR);
+        mTextInputEditTextConfirmPassword= findViewById(R.id.textInputEditTextConfirmPassword);
+        mButtonRegister= findViewById(R.id.btnregister);
+
+        mButtonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register();
+            }
+        });
+
+
         mCircleImageViewBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,6 +52,48 @@ public class RegisterActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void register() {
+        //metodo resgitro
+        String username=mTextInputEditTextUsername.getText().toString();
+        String email=mTextInputEditTextEmailR.getText().toString();
+        String password=mTextInputEditTextPasswordR.getText().toString();
+        String confirmpassword=mTextInputEditTextConfirmPassword.getText().toString();
+
+        if (!username.isEmpty() && !email.isEmpty() && !password.isEmpty() && !confirmpassword.isEmpty()){
+            if (isEmailValid(email)){
+                if(password.equals(confirmpassword)){
+                    if(password.length() >=6){
+                        createUser(email,password,username);
+                    }else {
+                        Toast.makeText(this, "La Contraseña debe tener 6 caracteres", Toast.LENGTH_SHORT).show();
+                    }
+
+                }else{
+                    Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+                }
+
+            }else {
+                Toast.makeText(this, "Has insertado todos los campos pero el correo no es valido", Toast.LENGTH_SHORT).show();
+            }
+
+
+        }else {
+            Toast.makeText(this, "para continuar inserta todos los campos", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void createUser(String email, String password, String username) {
+
+    }
+
+    public boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 
 }
